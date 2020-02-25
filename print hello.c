e70421a58bfe5beebdfb964f4f9fde4cc58bd3ba
Abhishek Kumar <advikydv@gmail.com>
2020-02-25 21:25:20 +0530

    Add files via upload

---
+++ print hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+/**********************************************
+Author : Abhishek Kuamr
+Purpose : to print hello
+*********************************************/s
+void main ()
+{
+printf(" Hello Advik Welcome to Programming World :");
+getch();
+}
